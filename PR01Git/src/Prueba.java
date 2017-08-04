a3f849f09e55e8b68640f99e7c4f3ce1a7ffaf4f
Montse Viyuela <montsemsanz@gmail.com>
2017-08-04 13:30:04 +0200

    modified Prueba y add Prueba2

--- PR01Git/src/Prueba.java
+++ PR01Git/src/Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Primera línea");
-		
+		System.out.println("Segunda línea");
 		
 
 	}
---
+++ PR01Git/src/Prueba2.java
@@ -0,0 +1,11 @@
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		System.out.println("Primera línea en Prueba2");
+		System.out.println("Segunda línea en Prueba2");
+		
+
+	}
+
+}
