4686fab9d74b62ef7b6420e4610498b1b63bd3b2
Montse Viyuela <montseaweb@gmail.com>
2019-04-08 11:13:02 +0200

    Modificados Prueba y Prueba2

--- PR01Git/src/Prueba.java
+++ PR01Git/src/Prueba.java
@@ -2,7 +2,7 @@
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Probando Eclipse con Git");
+		System.out.println("Probando de nuevo Eclipse con Git");
 		System.out.println("Segunda línea");
 		
 
--- PR01Git/src/Prueba2.java
+++ PR01Git/src/Prueba2.java
@@ -4,8 +4,8 @@ public class Prueba2 {
 	public static void main(String[] args) {
 		System.out.println("Primera línea en Prueba2");
 		System.out.println("Segunda línea en Prueba2");
-		System.out.println("Tercera línea en Prueba2");
-		System.out.println("Cuarta línea en Prueba2");
+		 
+		 
 
 	}
 
